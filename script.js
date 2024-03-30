97401980fa43d1604f73d71c7addd0a403acc711
AleFucar <alessandrofucarino02@gmail.com>
2024-03-30 23:12:16 +0100

    ADD: refactoring del calcolo del prezzo e aggiunta funzione per stampare il prezzo html

--- script.js
+++ script.js
@@ -19,46 +19,55 @@ function invioForm(e) {
 
     e.preventDefault()
 
-    console.log("Pulsante cliccato!")
-
-
+    
+    //console.log("Pulsante cliccato!")
 
     calcolaPrezzo()
 
-    calcolaSconto()
 
 }
 
+
+const codiceElement = document.getElementById('codiceForm') 
+const codiciSconto = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
 
 function calcolaPrezzo() {
      
     if (select.value == 0) {
         console.log("non hai selezionato il lavoro")
-        return
+        return false
     } else if (select.value == 1) {
         prezzoFinale = oreDiLavoro * 20.50
-        console.log(prezzoFinale)
     } else if (select.value == 2) {
         prezzoFinale = oreDiLavoro * 15.30
-        console.log(prezzoFinale)
     } else if (select.value == 3) {
         prezzoFinale = oreDiLavoro * 33.60
+    }
+
+    if (codiciSconto.includes(codiceElement.value)) {
+        const sconto = (prezzoFinale * 25)/100
+        const prezzoScontato = prezzoFinale - sconto
+        prezzoFinale = prezzoScontato
+        console.log('Prezzo scontato: ' + prezzoScontato)
+        stampaPrezzo()
+    } else {
+        stampaPrezzo()
         console.log(prezzoFinale)
+        console.log('Codice Promozionale non inserito o errato')
     }
 
 }
 
-const codiceElement = document.getElementById('codiceForm') 
-const codiciSconto = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
 
-function calcolaSconto() {
-    
-    if (codiciSconto.includes(codiceElement.value)) {
-        const sconto = (prezzoFinale * 25)/100
-        const prezzoScontato = prezzoFinale - sconto
-        console.log('Prezzo scontato: ' + prezzoScontato)
-    } else {
-        console.log('nessuno sconto')
-    }
 
+function stampaPrezzo() {
+
+    const prezzoStampaElement = document.getElementById('prezzoFinale')
+
+    const prezzoFinito = 
+    `
+    &euro;${prezzoFinale}
+    <span class="prezzoGrigio"></span>
+    `
+    prezzoStampaElement.innerHTML = prezzoFinito
 }
